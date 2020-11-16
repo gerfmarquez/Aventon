210cf9417ac481988a7bc09bd87f5ce2badc442b
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:55:29 -0500

    Update GoogleMapsModelRequestBuilder.java

--- app/src/main/java/com/smidur/aventon/utilities/GoogleMapsModelRequestBuilder.java
+++ app/src/main/java/com/smidur/aventon/utilities/GoogleMapsModelRequestBuilder.java
@@ -9,9 +9,12 @@ import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
 
-/**
- * Created by marqueg on 11/16/15.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class GoogleMapsModelRequestBuilder {
     /**
      * Takes list of locations to build the route calculation request
