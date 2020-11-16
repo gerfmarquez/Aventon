14f3954e5e69e70d96a0d47733f614e155e33078
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:50:09 -0500

    Update GoogleApiRoute.java

--- app/src/main/java/com/smidur/aventon/model/GoogleApiRoute.java
+++ app/src/main/java/com/smidur/aventon/model/GoogleApiRoute.java
@@ -1,8 +1,11 @@
 package com.smidur.aventon.model;
 
-/**
- * Created by marqueg on 11/16/15.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
+
 public class GoogleApiRoute {
     GoogleApiPolyline overview_polyline;
     GoogleApiBound bounds;
