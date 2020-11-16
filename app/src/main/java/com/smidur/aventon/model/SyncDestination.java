c6fbfff6e3ce916bbe86de831c6d42de4e1b6d8f
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:51:21 -0500

    Update SyncDestination.java

--- app/src/main/java/com/smidur/aventon/model/SyncDestination.java
+++ app/src/main/java/com/smidur/aventon/model/SyncDestination.java
@@ -1,7 +1,9 @@
 package com.smidur.aventon.model;
 
-/**
- * Created by marqueg on 5/15/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
 
 public class SyncDestination {
