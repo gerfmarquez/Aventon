5e3aa56df6b353b3b758996fad2033f632fc3052
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:54:26 -0500

    Update FareUtil.java

--- app/src/main/java/com/smidur/aventon/utilities/FareUtil.java
+++ app/src/main/java/com/smidur/aventon/utilities/FareUtil.java
@@ -6,8 +6,10 @@ import android.util.Log;
 
 import java.util.Calendar;
 
-/**
- * Created by marqueg on 7/3/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
 
 public class FareUtil {
