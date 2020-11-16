83ec3499138c8d2504ecc9523bfc95df7ba19f41
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:44:52 -0500

    Update IdentityDemoFragment.java

--- app/src/main/java/com/smidur/aventon/demo/IdentityDemoFragment.java
+++ app/src/main/java/com/smidur/aventon/demo/IdentityDemoFragment.java
@@ -18,6 +18,11 @@ import com.amazonaws.mobile.AWSMobileClient;
 import com.amazonaws.mobile.user.IdentityManager;
 import com.smidur.aventon.R;
 
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
+ */
 
 public class IdentityDemoFragment extends Fragment implements IdentityManager.SignInStateChangeListener {
     /** Logging tag for this class. */
