f5234949ea7c56d7d169107a2ae027311bf5981a
Gerardo Marquez <gerfmarquez@hotmail.com>
2020-11-16 02:45:56 -0500

    Update TokenInvalidException.java

--- app/src/main/java/com/smidur/aventon/exceptions/TokenInvalidException.java
+++ app/src/main/java/com/smidur/aventon/exceptions/TokenInvalidException.java
@@ -1,7 +1,9 @@
 package com.smidur.aventon.exceptions;
 
-/**
- * Created by marqueg on 5/10/17.
+/** This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * Copyright 2020, Gerardo Marquez.
  */
 
 public class TokenInvalidException extends Exception {
